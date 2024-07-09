4f0b0f424c3e5f011aa18e96b234b4b2d9f24494
Yuri Pereira de Jesus <yuripjes@hotmail.com>
2024-07-09 15:27:01 -0300

    Ajustes na formatação e remoção de comentários

--- unificador.js
+++ unificador.js
@@ -2,46 +2,27 @@ const fs = require("fs");
 
 prepararArquivo();
 
+function prepararArquivo() {
+  //TODO rodar sanitizer no html gerado
+  let html = `<div id="app-calc-ip"></div>`;
 
-function prepararArquivo(){
-    //gerar com funçoes proprias do html > Sanitizar > depois pegar o innerhtml
-    //const parser = new DOMParser();
-    let html = `<div id="app-calc-ip"></div>`
+  html += gerarTagScript(lerArquivo("./app/calculadora-indice-prioridade.js"));
 
-    html += gerarTagScript(lerArquivo('./app/calculadora-indice-prioridade.js'));
-    
-
-
-    //console.log('######### html final ######\n', html)
-    escreverHtml(html);
+  //console.log('######### html final ######\n', html)
+  escreverArquivoHtml(html);
 }
 
 function lerArquivo(path) {
-    return fs.readFileSync(path, 'utf8')
-   /* Função asíncrona
-    let conteudo = '';
-    console.log('lerArquivo: ', path)
-    fs.readFile(path, 'utf-8', (err, data) /* callback *//* => {
-        if (err) {
-            //res.status(500).send(err);
-            console.error('Erro ao ler arquivo', err)
-            return;
-        }
-        console.log('Conteúdo do arquivo:', data);
-        return data;
-    });
-    return conteudo;
-
-    */
+  return fs.readFileSync(path, "utf8");
 }
 
-function gerarTagScript(conteudo){
-return `\n<script>\n${conteudo}\n</script>`
+function gerarTagScript(conteudo) {
+  return `\n<script>\n${conteudo}\n</script>`;
 }
 
-function escreverHtml(html){
-    fs.writeFile("arquivo-publicacao.html", html, (err) => {
-        if (err) throw err;
-        console.info("O arquivo foi criado com sucesso!");
-      });
+function escreverArquivoHtml(html) {
+  fs.writeFile("arquivo-publicacao.html", html, (err) => {
+    if (err) throw err;
+    console.info("O arquivo foi criado com sucesso!");
+  });
 }
