99946197b45d4c922f959462fa9ec169c94931d8
yuripjes <54251911+yuripjes@users.noreply.github.com>
2024-08-09 12:15:38 -0300

    Melhorias no seletor de carga horária (#2)
    * Ajusta nome de variavel no v-for
    
    * Inclui a opção outros
    
    * Define foco no input ao selecionar a opção Outra CH
    
    ---------
    
    Co-authored-by: Yuri Pereira de Jesus <yuripjes@hotmail.com>

--- app/components/card-nova-disciplina.js
+++ app/components/card-nova-disciplina.js
@@ -12,18 +12,12 @@ Vue.component("card-nova-disciplina", {
     };
   },
   computed: {
-    desativarSeletorCH: function () {
-      let ch = String(this.itemPeriodo.novoItemDisciplina.cargaHoraria);
-      return ch.length > 0;
-    },
-    desativarInputCH:  function () {
-      let ch =this.chSelecionada;
-      return ch !== null;
+    exibirInputCH:  function () {
+      return this.chSelecionada === 'OUTRA_CH';
     },
     desativarBotaoAdicionar: function () {
-      let chInp = Number(this.itemPeriodo.novoItemDisciplina.cargaHoraria);
-      let chSel = Number(this.chSelecionada);
-
+      let chInp = Number(this.itemPeriodo.novoItemDisciplina.cargaHoraria) || 0;
+      let chSel = Number(this.chSelecionada) || 0;
       let situacao = this.itemPeriodo.novoItemDisciplina.situacao
 
       return (chInp === 0 && chSel === 0) || situacao === null;
@@ -45,6 +39,11 @@ Vue.component("card-nova-disciplina", {
     definirCargaHoraria(novoItemDisciplina, cargaHoraria) {
       novoItemDisciplina.cargaHoraria = Number(cargaHoraria);
     },
+    onOutraChSelected(key) {
+      this.$nextTick(()=>{
+      this.$refs[key].focus();
+      });
+    }
   },
   template: `
     <div style="background: #daffff; width: 100%; padding: 2rem; display: flex; flex-direction: row; gap: 10px; ">
@@ -52,21 +51,23 @@ Vue.component("card-nova-disciplina", {
       <div style=" display: flex; flex-direction: column; gap: 10px;">
           <label>Informe a carga horária do componente <b>cursado</b></label>
           <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-            <template v-for="(item, index) in chPredefinidaOptions">
-                <input type="radio" class="btn-check" :name="'btnradioCh-'+indexPeriodo" :id="'btnradioCh-'+indexPeriodo+'-'+index" autocomplete="off" v-model="chSelecionada" :value="item" :disabled="desativarSeletorCH">
-                <label class="btn btn-outline-primary" :for="'btnradioCh-'+indexPeriodo+'-'+index">{{item}}</label>
+            <template v-for="(item, indexChPredefinida) in chPredefinidaOptions">
+                <input type="radio" class="btn-check" :name="'btnradioCh-'+indexPeriodo" :id="'btnradioCh-'+indexPeriodo+'-'+indexChPredefinida" autocomplete="off" v-model="chSelecionada" :value="item">
+                <label class="btn btn-outline-primary" :for="'btnradioCh-'+indexPeriodo+'-'+indexChPredefinida">{{item}}</label>
             </template>
+            <input type="radio" class="btn-check" :name="'btnradioCh-'+indexPeriodo" :id="'btnradioCh-'+indexPeriodo+'-outros'" autocomplete="off" v-model="chSelecionada" :value="'OUTRA_CH'" @change="onOutraChSelected('NOVO_inputCHDisciplinas_'+indexPeriodo+'_Ref')">
+            <label class="btn btn-outline-primary" :for="'btnradioCh-'+indexPeriodo+'-outros'">Outra CH</label>
           </div>
-          <input type="number" v-model.number="itemPeriodo.novoItemDisciplina.cargaHoraria" :disabled="desativarInputCH" min="1" max="9999"
+          <input v-if="exibirInputCH" type="number" v-model.number="itemPeriodo.novoItemDisciplina.cargaHoraria"  min="1" max="9999" placeholder="Digite a carga horária"
           :ref="'NOVO_inputCHDisciplinas_'+indexPeriodo+'_Ref'"></input>
       </div>
 
       <div>
           <label>Informe a situação do componente <b>cursado</b></label>
           <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-            <template v-for="(item, index) in situacaoOptions">
-                <input type="radio" class="btn-check" :name="'btnradioSitacao'+indexPeriodo" :id="'btnradioSitacao-'+indexPeriodo+'-'+index" autocomplete="off" v-model="itemPeriodo.novoItemDisciplina.situacao" :value="item">
-                <label class="btn btn-outline-primary" :for="'btnradioSitacao-'+indexPeriodo+'-'+index">{{item}}</label>
+            <template v-for="(item, indexSituacoes) in situacaoOptions">
+                <input type="radio" class="btn-check" :name="'btnradioSitacao'+indexPeriodo" :id="'btnradioSitacao-'+indexPeriodo+'-'+indexSituacoes" autocomplete="off" v-model="itemPeriodo.novoItemDisciplina.situacao" :value="item">
+                <label class="btn btn-outline-primary" :for="'btnradioSitacao-'+indexPeriodo+'-'+indexSituacoes">{{item}}</label>
             </template>
           </div>
       </div>
